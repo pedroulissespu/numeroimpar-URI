1fb652dc35be262da413faf1ea1e8c6a87f13a34
Pedro Ulisses Pereira Castro Maia <pedro.ulisses@aluno.uece.br>
2021-09-27 19:28:33 -0300

    Numero Impares URI

---
+++ impar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num,num2;
+    cin >> num;
+        for( num2=1; num2 <= num ;num2++){
+            cout << num2 << endl;
+            num2++;
+        }
+        return 0;
+}
